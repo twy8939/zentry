111d1079e942254a5bf4e09050b8a155be0e8436
twy8939 <twy8939@naver.com>
2025-02-23 02:08:03 +0900

    init: Add Tailwind Config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl text-orange-500 font-bold">Welcome</h1>
+      <h1 className="text-5xl font-bold text-violet-300">Welcome</h1>
     </main>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,32 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        zentry: ["zentry", "sanf-serif"],
+        general: ["general", "sanf-serif"],
+        "circular-web": ["circular-web", "sanf-serif"],
+        "robert-medium": ["robert-medium", "sanf-serif"],
+        "robert-regular": ["robert-regular", "sanf-serif"],
+      },
+      colors: {
+        blue: {
+          50: "#DFDFF0",
+          75: "#DFDFF2",
+          100: "#F0F2FA",
+          200: "#010101",
+          300: "#4FB7DD",
+        },
+        violet: {
+          300: "#5724FF",
+        },
+        yellow: {
+          100: "#8E983F",
+          300: "#EDFF66",
+
+        }
+      }
+    },
   },
   plugins: [],
 }
