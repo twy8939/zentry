8ceeea2067f41794c7b3f5010a7b80d6940f91c7
twy8939 <twy8939@naver.com>
2025-02-28 19:02:16 +0900

    feat : Add clipAnimation for about

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import About from "./components/About";
 import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <main className="relative w-screen min-h-screen overflow-x-hidden">
       <Hero />
+      <About />
 
       <section className="z-0 min-h-screen bg-blue-500" />
     </main>
